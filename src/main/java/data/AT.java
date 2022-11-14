67d8072939a38d79f9abe1276668ae9e8a6dfe9f
jutnh <jutnhbr@gmail.com>
2022-11-14 15:18:58 +0100

    Added more AT Commands

--- src/main/java/data/AT.java
+++ src/main/java/data/AT.java
@@ -2,13 +2,14 @@ package data;
 
 public enum AT {
 
-    AT_TEST("AT"),
+    AT("AT"),
     AT_CFG("AT+CFG="),
     AT_RST("AT+RST"),
     AT_VER("AT+VER"),
     AT_SEND("AT+SEND="),
     AT_RSSI("AT+RSSI?"),
-    AT_RX("AT+RX");
+    AT_RX("AT+RX"),
+    AT_ADDR("AT+ADDR?");
 
     private final String command;
 
