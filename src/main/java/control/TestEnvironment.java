2f94056c2e061fc089a5c2050dba8ef9403a39d2
jutnhbr <jutnhbr@gmail.com>
2022-12-15 16:27:45 +0100

    [+] Testing

--- src/main/java/control/TestEnvironment.java
+++ src/main/java/control/TestEnvironment.java
@@ -12,10 +12,11 @@ public class TestEnvironment {
         serialTester.connect(serialTester.getPortByName(console.readStringFromInput("Enter Port Name: ")));
         console.printMessage("Tester connected to "+ serialTester.getActivePort().getSystemPortName() + "\n");
         console.printMessage("Starting Parsing Thread...\n");
-        ParseTesterThread test = new ParseTesterThread(serialTester);
-        test.start();
-        console.printMessage("Tester is running ...\n");
-
+        // ParseTesterThread test = new ParseTesterThread(serialTester);
+        // test.start();
+        // console.printMessage("Tester is running ...\n");
+        Thread.sleep(4000);
+        serialTester.writeData("LR,XXXX,XX,000001000000000001000000111111111111111100000001101011011101110100011001");
 
 
 
