9722916e985f7b86f97c3a46ff14694fff6b1376
Justin <jutnhbr@gmail.com>
2023-01-04 15:39:06 +0100

    [+] Added DestAddr to DATA

--- src/main/java/model/messageTypes/DATA.java
+++ src/main/java/model/messageTypes/DATA.java
@@ -6,10 +6,12 @@ public class DATA {
 
     private final BitString type = new BitString("000000");
     private BitString message;
+    private BitString destAddr;
 
 
-    public DATA(BitString message) {
+    public DATA(BitString message, BitString destAddr) {
         this.message = message;
+        this.destAddr = destAddr;
     }
 
     public BitString getType() {
@@ -20,11 +22,16 @@ public class DATA {
         return message;
     }
 
+    public BitString getDestAddr() {
+        return destAddr;
+    }
+
     @Override
     public String toString() {
         return "DATA{" +
                 "type=" + type +
                 ", message=" + message +
+                ", destAddr=" + destAddr +
                 '}';
     }
 }
